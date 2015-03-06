a069f8e3e1c6f9b2c4ff16a4f787cc75e5cf0a17
gonechar <gonechar@gmail.com>
2015-03-06 23:46:56 +0600

    06.03.23_45
    Закончив первую версию тестов и отредактировав проект выкладываю

--- tp.lab1/filew.cpp
+++ tp.lab1/filew.cpp
@@ -101,26 +101,27 @@ char filew::parseCMNDArg(char *procA)
 {
 
 	if (strcmp(procA, "GENERATE") == 0 || strcmp(procA, "Generate") == 0 || strcmp(procA, "generate") == 0 || strcmp(procA, "CREATE") == 0 || strcmp(procA, "create") == 0 || strcmp(procA, "Create") == 0){
-		cout << "SUCCESS \tPROC " << procA << "\t*PROC " << *procA << "\n";
+		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		return 'G';
 	}
 	if (strcmp(procA, "DEL") == 0 || strcmp(procA, "del") == 0 || strcmp(procA, "Del") == 0 || strcmp(procA, "DELETE") == 0 || strcmp(procA, "delete") == 0 || strcmp(procA, "Delete") == 0){
-		cout << "SUCCESS \tPROC " << procA << "\t*PROC " << *procA << "\n";
+		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		return 'D';
 	}
 	if (strcmp(procA, "RENAME") == 0 || strcmp(procA, "rename") == 0 || strcmp(procA, "Rename") == 0){
-		cout << "SUCCESS \tPROC " << procA << "\t*PROC " << *procA << "\n";
+		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		return 'R';
 	}
 	if (strcmp(procA, "COPY") == 0 || strcmp(procA, "copy") == 0 || strcmp(procA, "Copy") == 0){
-		cout << "SUCCESS \tPROC " << procA << "\t*PROC " << *procA << "\n";
+		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		return 'C';
 	}
 	if (strcmp(procA, " ") == 0 || strcmp(procA, "") == 0 ){
-		cout << "EMPTY \tPROC " << procA << "\t*PROC " << *procA << "\n";
+		cout << "EMPTY_ARGS \t First argument [" << procA << "]\t First letter is " << *procA << "\n";
 		return 'N';
 	}
-	cout << "FAULT CMND \tPROC " << procA << "\t*PROC " << *procA << "\n";
+	if (strcmp(procA, "ABZ") == 0)	return 'A';
+	cout << "FAULT_CMND_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 	return 'E';
 	system("pause");
 }
@@ -134,15 +135,16 @@ void filew::getproc(char *fproc,char *str1,char *str2,filew *file,int argentum)
 
 	char zArg = filew::parseCMNDArg(fproc);
 	
-	switch (proc[0])
+	switch (zArg)
 	{
-	case 'G':{if (argentum < 3){ cout << "d)argument error" << endl; return; } file->fcreate(); return; }
-	case 'D':{if (argentum < 3){ cout << "d)argument error" << endl; return; }file->fdelete(); return; }
-	case 'C':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->fcopy(); return; }
-	case 'R':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->frename(); return; }
-	case 'N':cout << "e)command was not given" << endl; return;
-	case 'E':cout << "E)WRONG command was given" << endl; return;
-	default: cout << "f)404 illegal command not found" << endl; return;
+	case 'G':{if (argentum < 3){ cout << "d)second argument error" << endl; return; } file->fcreate(); return; }
+	case 'D':{if (argentum < 3){ cout << "d)second argument error" << endl; return; }file->fdelete(); return; }
+	case 'C':{if (argentum < 4){ cout << "d)second argument error" << endl; return; }file->fcopy(); return; }
+	case 'R':{if (argentum < 4){ cout << "d)second argument error" << endl; return; }file->frename(); return; }
+	case 'N': {cout << "e)command was not given" << endl; return; }
+	case 'E': {cout << "E)510 WRONG command was given" << endl; return; }
+	case 'A': {cout << "______________________________________________________\n\n"; return; }
+	default: {cout << "f)404 illegal command not found" << endl; return; }
 	}
 	
 }
