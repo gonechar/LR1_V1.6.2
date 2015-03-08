f31b3ea56a2ad57dc655eefef8925a33378e0ac7
gonechar <gonechar@gmail.com>
2015-03-08 20:54:59 +0600

    Допил после сдачи лабы 1

--- tp.lab1/argsParse.cpp
+++ tp.lab1/argsParse.cpp
@@ -4,23 +4,25 @@ using namespace std;
 
 char argsParsing::getCMNDArg(char *procA)
 {
-
-	if (strcmp(procA, "GENERATE") == 0 || strcmp(procA, "Generate") == 0 || strcmp(procA, "generate") == 0 || strcmp(procA, "CREATE") == 0 || strcmp(procA, "create") == 0 || strcmp(procA, "Create") == 0){
+	for (int i = 0; i < strlen(procA);i++){
+		procA[i] = tolower(procA[i]);
+	}
+	if (strcmp(procA, "generate") == 0 || strcmp(procA, "create") == 0){
 		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		this->CMNDarg='G';
 		return 'G';
 	}
-	if (strcmp(procA, "DEL") == 0 || strcmp(procA, "del") == 0 || strcmp(procA, "Del") == 0 || strcmp(procA, "DELETE") == 0 || strcmp(procA, "delete") == 0 || strcmp(procA, "Delete") == 0){
+	if (strcmp(procA, "del") == 0 || strcmp(procA, "delete") == 0){
 		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		this->CMNDarg = 'D';
 		return 'D';
 	}
-	if (strcmp(procA, "RENAME") == 0 || strcmp(procA, "rename") == 0 || strcmp(procA, "Rename") == 0){
+	if (strcmp(procA, "rename") == 0){
 		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		this->CMNDarg = 'R';
 		return 'R';
 	}
-	if (strcmp(procA, "COPY") == 0 || strcmp(procA, "copy") == 0 || strcmp(procA, "Copy") == 0){
+	if (strcmp(procA, "copy") == 0){
 		cout << "SUCCESS_FIRST_ARG \t First argument is [" << procA << "]\t First letter is " << *procA << "\n";
 		this->CMNDarg = 'C';
 		return'C';
@@ -30,12 +32,12 @@ char argsParsing::getCMNDArg(char *procA)
 		this->CMNDarg = 'N';
 		return 'N';
 	}
-	if (strcmp(procA, "ISEXIST") == 0) {
+	if (strcmp(procA, "isexist") == 0) {
 		cout << "SUCCESS_FIRST_ARG \t First argument [" << procA << "]\t First letter is " << *procA << "\n";
 		this->CMNDarg = 'S';
 		return 'S';
 	}
-	if (strcmp(procA, "ABZ") == 0) {
+	if (strcmp(procA, "abz") == 0) {
 		this->CMNDarg = 'A';
 		return 'A';
 	}
